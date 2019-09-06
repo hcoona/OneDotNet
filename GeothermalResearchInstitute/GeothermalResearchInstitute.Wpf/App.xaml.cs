b47dc763e952008535bc655abbece33aa42100a5
Shuai Zhang <zhangshuai.ustc@gmail.com>
2019-09-06 22:27:08 +0800

    Add GeothermalResearchInstitute.Wpf

---
+++ GeothermalResearchInstitute/GeothermalResearchInstitute.Wpf/App.xaml.cs
@@ -0,0 +1,22 @@
+// <copyright file="App.xaml.cs" company="Shuai Zhang">
+// Copyright Shuai Zhang. All rights reserved.
+// Licensed under the GPLv3 license. See LICENSE file in the project root for full license information.
+// </copyright>
+
+using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace GeothermalResearchInstitute.Wpf
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+    }
+}

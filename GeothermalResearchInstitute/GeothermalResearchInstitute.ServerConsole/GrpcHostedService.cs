e065716a7463b5c3a7ff3a1f394b37f87242935c
Shuai Zhang <zhangshuai.ustc@gmail.com>
2019-09-12 23:02:48 +0800

    [GeothermalResearchInstitute] Launch ServerConsole with Hosting extension.

---
+++ GeothermalResearchInstitute/GeothermalResearchInstitute.ServerConsole/GrpcHostedService.cs
@@ -0,0 +1,45 @@
+using System.Linq;
+using System.Threading;
+using System.Threading.Tasks;
+using Grpc.Core;
+using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
+
+namespace GeothermalResearchInstitute.ServerConsole
+{
+    internal class GrpcHostedService : IHostedService
+    {
+        private readonly ILogger<GrpcHostedService> logger;
+        private readonly Server server;
+
+        public GrpcHostedService(ILogger<GrpcHostedService> logger, Server server)
+        {
+            this.logger = logger;
+            this.server = server;
+        }
+
+        public Task StartAsync(CancellationToken cancellationToken)
+        {
+            if (cancellationToken.IsCancellationRequested)
+            {
+                return Task.FromCanceled(cancellationToken);
+            }
+
+            this.server.Start();
+            if (this.logger.IsEnabled(LogLevel.Information))
+            {
+                this.logger.LogInformation(
+                    "Grpc services are listening on {}",
+                    string.Join(",", this.server.Ports.Select(p => $"{p.Host}:{p.Port}")));
+            }
+
+            return Task.CompletedTask;
+        }
+
+        public Task StopAsync(CancellationToken cancellationToken)
+        {
+            this.logger.LogInformation("Killing grpc server...");
+            return this.server.KillAsync();
+        }
+    }
+}

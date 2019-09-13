d000e9e48db51b9d8f391bee97607d599cf6ea11
Shuai Zhang <zhangshuai.ustc@gmail.com>
2019-09-13 12:35:34 +0800

    [GeothermalResearchInstitute] Add binding credentials in appsettings.ini

---
+++ GeothermalResearchInstitute/GeothermalResearchInstitute.ServerConsole/Credential.cs
@@ -0,0 +1,25 @@
+// <copyright file="Program.cs" company="Shuai Zhang">
+// Copyright Shuai Zhang. All rights reserved.
+// Licensed under the GPLv3 license. See LICENSE file in the project root for full license information.
+// </copyright>
+
+using GeothermalResearchInstitute.v1;
+
+namespace GeothermalResearchInstitute.ServerConsole
+{
+    public class Credential
+    {
+        public string Nickname { get; set; }
+
+        public string Username { get; set; }
+
+        public string Password { get; set; }
+
+        public UserRole Role { get; set; }
+
+        public override string ToString()
+        {
+            return Nickname + "(" + Username + ":" + Role + ")";
+        }
+    }
+}

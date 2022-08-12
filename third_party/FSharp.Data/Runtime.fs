412bdbf8e0cc53d60e4ca256496d5cc4396846ab
Shuai Zhang <zhangshuai.ustc@gmail.com>
2022-08-12 19:43:09 +0800

    Use community upgraded FSharp.Data

--- third_party/FSharp.Data/Runtime.fs
+++
@@ -1,8 +0,0 @@
-﻿namespace global
-
-open System.Runtime.CompilerServices
-open FSharp.Core.CompilerServices
-
-[<assembly:TypeProviderAssembly("FSharp.Data.DesignTime")>]
-[<assembly:InternalsVisibleToAttribute("FSharp.Data.Tests")>]
-do()

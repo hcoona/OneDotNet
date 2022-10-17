cae0684952ab14491115f35905439003f72e9726
Shuai Zhang <zhangshuai.ustc@gmail.com>
2022-10-17 18:47:34 +0800

    chore: fix license headers

--- srcs/RateLimiter/GlobalSuppressions.cs
+++ srcs/RateLimiter/GlobalSuppressions.cs
@@ -1,7 +1,20 @@
-// <copyright file="GlobalSuppressions.cs" company="Shuai Zhang">
-// Copyright Shuai Zhang. All rights reserved.
-// Licensed under the GPLv3 license. See LICENSE file in the project root for full license information.
-// </copyright>
+// Copyright (c) 2022 Zhang Shuai<zhangshuai.ustc@gmail.com>.
+// All rights reserved.
+//
+// This file is part of OneDotNet.
+//
+// OneDotNet is free software: you can redistribute it and/or modify it under
+// the terms of the GNU General Public License as published by the Free
+// Software Foundation, either version 3 of the License, or (at your option)
+// any later version.
+//
+// OneDotNet is distributed in the hope that it will be useful, but WITHOUT ANY
+// WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+// FOR A PARTICULAR PURPOSE. See the GNU General Public License for more
+// details.
+//
+// You should have received a copy of the GNU General Public License along with
+// OneDotNet. If not, see <https://www.gnu.org/licenses/>.
 //
 // This file is used by Code Analysis to maintain SuppressMessage
 // attributes that are applied to this project.
@@ -14,3 +27,4 @@ using System.Diagnostics.CodeAnalysis;
 [assembly: SuppressMessage("StyleCop.CSharp.MaintainabilityRules", "SA1401:Fields should be private", Justification = "<挂起>", Scope = "member", Target = "~F:RateLimiter.SmoothRateLimiter.storedPermits")]
 [assembly: SuppressMessage("StyleCop.CSharp.MaintainabilityRules", "SA1401:Fields should be private", Justification = "<挂起>", Scope = "member", Target = "~F:RateLimiter.SmoothRateLimiter.maxPermits")]
 [assembly: SuppressMessage("StyleCop.CSharp.MaintainabilityRules", "SA1401:Fields should be private", Justification = "<挂起>", Scope = "member", Target = "~F:RateLimiter.SmoothRateLimiter.stableInterval")]
+[assembly: SuppressMessage("StyleCop.CSharp.DocumentationRules", "SA1636:File header copyright text should match", Justification = "Reviewed")]

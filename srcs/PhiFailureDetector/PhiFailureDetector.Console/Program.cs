07e189bd4d7276be3a968de43ca2f62e2434df86
Shuai Zhang <zhangshuai.ustc@gmail.com>
2022-10-17 18:47:34 +0800

    chore: fix license headers

--- srcs/PhiFailureDetector/PhiFailureDetector.Console/Program.cs
+++ srcs/PhiFailureDetector/PhiFailureDetector.Console/Program.cs
@@ -1,7 +1,20 @@
-// <copyright file="Program.cs" company="Shuai Zhang">
-// Copyright Shuai Zhang. All rights reserved.
-// Licensed under the GPLv3 license. See LICENSE file in the project root for full license information.
-// </copyright>
+// Copyright (c) 2022 Zhang Shuai<zhangshuai.ustc@gmail.com>.
+// All rights reserved.
+//
+// This file is part of OneDotNet.
+//
+// OneDotNet is free software: you can redistribute it and/or modify it under
+// the terms of the GNU General Public License as published by the Free
+// Software Foundation, either version 3 of the License, or (at your option)
+// any later version.
+//
+// OneDotNet is distributed in the hope that it will be useful, but WITHOUT ANY
+// WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+// FOR A PARTICULAR PURPOSE. See the GNU General Public License for more
+// details.
+//
+// You should have received a copy of the GNU General Public License along with
+// OneDotNet. If not, see <https://www.gnu.org/licenses/>.
 
 using System;
 using System.Text;

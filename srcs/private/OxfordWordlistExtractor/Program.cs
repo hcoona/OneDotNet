9e0348c0523f4127d7462620ffdf51ef359c9b13
Shuai Zhang <zhangshuai.ustc@gmail.com>
2022-11-28 16:37:21 +0800

    chore(style): minor fixes in OxfordWordlistExtractor

--- srcs/private/OxfordWordlistExtractor/Program.cs
+++ srcs/private/OxfordWordlistExtractor/Program.cs
@@ -16,7 +16,6 @@
 // You should have received a copy of the GNU General Public License along with
 // OneDotNet. If not, see <https://www.gnu.org/licenses/>.
 
-using System.Collections.Immutable;
 using System.Net;
 using System.Text;
 using HtmlAgilityPack;

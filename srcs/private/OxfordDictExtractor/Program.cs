92d10776d8810f51c0220af90ddf5a1fa0118fb9
Shuai Zhang <zhangshuai.ustc@gmail.com>
2022-11-29 23:05:18 +0800

    feat: add oxford dict extractor project

---
+++ srcs/private/OxfordDictExtractor/Program.cs
@@ -0,0 +1,53 @@
+// Copyright (c) 2022 Zhang Shuai<zhangshuai.ustc@gmail.com>.
+// All rights reserved.
+//
+// This file is part of OneDotNet.
+//
+// OneDotNet is free software: you can redistribute it and/or modify it under
+// the terms of the GNU General Public License as published by the Free
+// Software Foundation, either version 3 of the License, or (at your option)
+// any later version.
+//
+// OneDotNet is distributed in the hope that it will be useful, but WITHOUT ANY
+// WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+// FOR A PARTICULAR PURPOSE. See the GNU General Public License for more
+// details.
+//
+// You should have received a copy of the GNU General Public License along with
+// OneDotNet. If not, see <https://www.gnu.org/licenses/>.
+
+using System.Collections.Immutable;
+using System.IO.Compression;
+using System.Net;
+using System.Text;
+using HtmlAgilityPack;
+
+var document = new HtmlDocument();
+using (var fs = File.OpenRead("oxford3000-5000.html"))
+using (var sr = new StreamReader(fs, Encoding.UTF8))
+{
+    document.Load(sr);
+}
+
+var words = (from node in document.DocumentNode.SelectNodes(
+                "//div[@id='wordlistsContentPanel']/ul/li")
+             select WebUtility.HtmlDecode(node.GetDataAttribute("hw").Value).Trim())
+            .ToImmutableSortedSet();
+
+var dict = new Dictionary<string, string>();
+using (var fs = ZipFile.OpenRead("dict.tsv.zip").Entries.Single().Open())
+using (var sr = new StreamReader(fs))
+{
+    string? line;
+    while ((line = sr.ReadLine()) != null)
+    {
+        var parts = line.Split('\t', 2, StringSplitOptions.TrimEntries);
+        if (words.Contains(parts[0]))
+        {
+            dict.Add(parts[0], parts[1]);
+        }
+    }
+}
+
+Console.WriteLine($"Load {dict.Count}/{words.Count} words.");
+Console.WriteLine($"The missing words are {string.Join(", ", words.Except(dict.Keys))}.");

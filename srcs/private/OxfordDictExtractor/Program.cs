12c13f0528b4bcd1d0a12f74368a2d96186bf3be
Shuai Zhang <zhangshuai.ustc@gmail.com>
2022-11-30 22:57:01 +0800

    feat: gen oxford3000 a1 words

--- srcs/private/OxfordDictExtractor/Program.cs
+++ srcs/private/OxfordDictExtractor/Program.cs
@@ -22,6 +22,7 @@ using System.Net;
 using System.Text;
 using OxfordDictExtractor;
 
+var words = new List<Word>();
 using (var fs = ZipFile.OpenRead("wordlist.tsv.zip").Entries.Single().Open())
 using (var sr = new StreamReader(fs))
 {
@@ -35,6 +36,28 @@ using (var sr = new StreamReader(fs))
             continue;
         }
 
-        Word.ParseFromDictContent(parts[0], parts[1]).PrintDebug(Console.Out);
+        words.Add(Word.ParseFromDictContent(parts[0], parts[1]));
     }
 }
+
+var wordClassEntries = from word in words
+                  from entry in word.Entries
+                  where !entry.IsOnlyPhrasalVerb && !entry.IsOnlyIdioms
+                  from sense in entry.Senses
+                  where !sense.IsXrefOnly
+                  where sense.CefrLevel == CefrLevel.A1
+                  group sense by new { entry.Name, entry.WordClass } into g
+                  select new
+                  {
+                      Name = g.Key.Name,
+                      WordClass = g.Key.WordClass,
+                      Senses = string.Join(
+                          "<br/>",
+                          g
+                              .OrderBy(s => s.SenseNumber)
+                              .Select(s => $"{s.SenseNumber}.&lt;{s.CefrLevel}&gt;{s.Grammar}{s.Labels}{s.ChineseDefinition}")),
+                  };
+var entries = from wce in wordClassEntries
+              group wce by wce.Name into g
+              select $"\"{g.Key}\",\"<ul>{string.Join(string.Empty, g.Select(wce => $"<li><p>{wce.WordClass}</p><p>{wce.Senses}</p></li>"))}</ul>\"";
+File.WriteAllLines("words.csv", entries, new UTF8Encoding(false));
